f59fbbe5a20e5f9118972a8106f20790926aaee0
ashubhankhar <157944297+ashubhankhar@users.noreply.github.com>
2024-12-24 19:03:59 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -61,7 +61,7 @@ class Bot(Client):
         self.set_parse_mode(ParseMode.HTML)
         self.LOGGER(__name__).info(f"Bot Running..!\n\nCreated by \nhttps://t.me/ultroid_official")
         self.LOGGER(__name__).info(f""" \n\n       
-(っ◔◡◔)っ ♥ Dramafilez ♥
+(っ◔◡◔)っ ♥ k-filez ♥
   ░╚════╝░░╚════╝░╚═════╝░
                                           """)
         self.username = usr_bot_me.username
